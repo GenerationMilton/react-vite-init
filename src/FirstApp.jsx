236dca7dd707e7da51a23ce5e9e1b6398e9e2c7f
Milton Muñoz <livemilton@gmail.com>
2024-09-29 20:34:23 -0500

    print variables in HTML with react

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,15 +1,30 @@
 
+//const newMessage= true;
+//const newMessage= "hola"
+//const newMessage= [1,2,3,4,5,6,7,8,9];
 
-//import { Fragment } from "react";
+//Print objects in functional component
+const newMessage={
+    message: 'Hola Mundo',
+    title:'Milton'
+}
+
+//function in react
+const getResult  =(a,b)=>{
+    return a+b;
+}
+
+//si se quiere imprimir el objeto se serializac con JSON.stringify
 
 //Fragmento agrupador de codigo jsx <> tienen un nodo padre
 //Por lo menos un nodo padre y devolver los elementos que se necesiten
 const FirstApp = () => {
+
   return (
     <>
-        <h1>
-        Milton Munoz
-        </h1>
+        <h1>{getResult(5,5)}</h1>
+        {/* <h1>Hola Mundo</h1> */}
+        {/* <code>{ JSON.stringify(newMessage) }</code> */}
         <p>Soy un subtitulo</p>
     </>
     
