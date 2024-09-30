d2ff10ef2ef01c8edb3c3ef2f40f78f92a0b2250
Milton Muñoz <livemilton@gmail.com>
2024-09-29 20:14:09 -0500

    return elements from component with fragments

---
+++ src/FirstApp.jsx
@@ -0,0 +1,19 @@
+
+
+//import { Fragment } from "react";
+
+//Fragmento agrupador de codigo jsx <> tienen un nodo padre
+//Por lo menos un nodo padre y devolver los elementos que se necesiten
+const FirstApp = () => {
+  return (
+    <>
+        <h1>
+        Milton Munoz
+        </h1>
+        <p>Soy un subtitulo</p>
+    </>
+    
+  )
+}
+
+export default FirstApp
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import HelloWorldApp from './HelloWorldApp';
-
-HelloWorldApp
+import FirstApp from './FirstApp';
+//import HelloWorldApp from './HelloWorldApp';
 // import HelloWorldApp from './HelloWorldApp';
 
 ReactDOM.createRoot( document.getElementById('root')).render(
     <React.StrictMode>
-        <HelloWorldApp />
+        <FirstApp />
     </React.StrictMode>
 )
