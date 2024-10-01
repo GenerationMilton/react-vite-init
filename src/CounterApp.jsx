243aec85b6be56fc1eff0af1239c1c0c693a5171
Milton Muñoz <livemilton@gmail.com>
2024-09-30 22:21:48 -0500

    hooks in react

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,10 +1,28 @@
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+//Event click
+// const handleAdd= (event)=>{
+//     console.log(event)
+// }
 
 export const CounterApp = ({value}) => {
+
+    const [ counter, setCounter] =useState(value);
+    //hooks
+    const handleAdd= ()=>{
+        // setCounter(counter +1);
+        setCounter((c)=>c+1);
+    }
+
   return (
     <>
       <h1>CounterApp</h1>
-      {value}
+      <h2>{counter}</h2>
+
+      <button onClick={ handleAdd}>
+        +1
+      </button>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,6 @@ import './styles.css';
 ReactDOM.createRoot( document.getElementById('root')).render(
     <React.StrictMode>
         {/* <FirstApp /> */}
-        <CounterApp value={10}/>
+        <CounterApp value={20}/>
     </React.StrictMode>
 );
