014ccb608350a5a46b2fe689a852703c46b171b2
Milton Muñoz <livemilton@gmail.com>
2024-10-03 23:04:42 -0500

    test to 05-funciones

--- src/bases/05-funciones.js
+++ src/bases/05-funciones.js
@@ -1,44 +1,12 @@
-// Funciones en JS
-
-const saludar = function ( nombre){
-    return `Hola, ${nombre}`;
-
-}
-
-const saludar2 = (nombre) =>{
-    return `Hola, ${nombre}`;
-};
-
-const saludar3 = (nombre) =>  `Hola, ${nombre}`;
-
-const saludar4= () => `Hola Mundo`;
-
-//console.log(saludar('Goku'))
-
-console.log(saludar);
-console.log(saludar2('Vegeta'));
-console.log(saludar2('Goku'));
-console.log(saludar4());
 
 
-const getUser = () =>({
+export const getUser = () =>({
     uid: 'ABC123',
     username:'El_Papi1502'
 });
 
-const user = getUser();
-console.log(user);
-
-// Tarea
-//1. Transformar a una funcion de flecha
-//2. Tiene que retornar un objeto implicito
-//3. Pruebas
-const getUsuarioActivo = (nombre) => 
+export const getUsuarioActivo = (nombre) => 
     ({
         uid:'abc567',
         username: nombre
     });
-
-//const usuarioActivo = getUsuarioActivo('Milton');
-const usuario = getUsuarioActivo('Jhon');
-console.log(usuario);
--- tests/base-pruebas/02-template-string.test.js
+++ tests/base-pruebas/02-template-string.test.js
@@ -1,4 +1,4 @@
-import { getSaludo } from '../../src/base-pruebas/02-template-string.js';
+import { getSaludo } from '../../src/bases/02-template-string';
 
 
 describe('Pruebas en 02-template-string', () => {
