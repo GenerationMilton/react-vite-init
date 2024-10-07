b1e10cf2e5818ee1a84a4dd627846decb093d499
Milton Muñoz <livemilton@gmail.com>
2024-10-06 22:45:44 -0500

    test in firstApp with react-library-test

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 
 //import HelloWorldApp from './HelloWorldApp';
 // import HelloWorldApp from './HelloWorldApp';
-import { CounterApp } from './CounterApp';
 import './styles.css';
+
+import { FirstApp } from './FirstApp';
 
 ReactDOM.createRoot( document.getElementById('root')).render(
     <React.StrictMode>
-        {/* <FirstApp /> */}
-        <CounterApp value={20}/>
+        <FirstApp title='Hola, Soy Milton' />
+        {/* <CounterApp value={20}/> */}
     </React.StrictMode>
 );
---
+++ tests/FirstApp.test.jsx
@@ -0,0 +1,26 @@
+import { render } from '@testing-library/react';
+import { FirstApp } from '../src/FirstApp';
+
+describe('Pruebas en <FirstApp />', () => {
+    
+    test('debe de hacer match con el snapshot', ()=> {
+
+        const title ='Hola, soy Goku';
+        const {container}= render(<FirstApp title={title} />)
+
+        expect(container).toMatchSnapshot();
+
+    });
+
+    test('should be to show the h1 title', ()=>{
+
+        const title ='Hola, soy Goku';
+        const {container, getByText}= render(<FirstApp title={title} />)
+
+        expect(getByText(title)).toBeTruthy();
+
+        const h1= container.querySelector('h1');
+        expect(h1.innerHTML).toContain(title);
+    });
+
+});
