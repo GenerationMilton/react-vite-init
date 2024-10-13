984e1a476cedace6b1e200e841eeabe3ca48e993
Milton Muñoz <livemilton@gmail.com>
2024-10-12 22:17:36 -0500

    test to counterApp

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,10 @@ import ReactDOM from 'react-dom/client';
 // import HelloWorldApp from './HelloWorldApp';
 import './styles.css';
 
-import { FirstApp } from './FirstApp';
 
 ReactDOM.createRoot( document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp title='Hola, Soy Milton' />
-        {/* <CounterApp value={20}/> */}
+        {/* <FirstApp title='Hola, Soy Milton' /> */}
+        <CounterApp value={20}/>
     </React.StrictMode>
 );
